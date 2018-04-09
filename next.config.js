0ea761a11d73d3d51d6d4c155c0b877543f1ef6a
Diana Baranova <Diana_Baranova@epam.com>
2018-04-09 14:52:47 +0300

    all linted

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 const withSass = require('@zeit/next-sass');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = withSass({
     cssModules: true,
     cssLoaderOptions: {
         importLoaders: 1,
         localIdentName: '[path]___[name]__[local]___[hash:base64:5]',
+    },
+    webpack(config, { dev }) {
+        if (dev) {
+            config.plugins.push(new StyleLintPlugin());
+        }
+
+        return config;
     },
 });
