8b1c9defc18733b071c76c8fe7bd4a2d7226f35c
Diana Baranova <Diana_Baranova@epam.com>
2018-04-09 11:41:29 +0300

    New test commit

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,10 @@ import React from 'react';
 
 import './styles.scss';
 
-const test = [
-    1,
-    2,
-    3]
+const test = [1, 2, 3];
 
 const Page = (props) => (
-    <div styleName="test">
-        Welcome bbb tongggggggggg gggggggggggggggggggg
-        ggggggggggggggggggggggggggggggext.js!!!!
-    </div>
+    <div styleName="test">Welcome bbb tongggggggggg gggggggggggggggggggg ggggggggggggggggggggggggggggggext.js!!!!</div>
 );
 
 export default Page;
