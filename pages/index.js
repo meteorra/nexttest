95603da99962fcb5dc116a891b5e5ea446b0bd78
Diana Baranova <Diana_Baranova@epam.com>
2018-04-09 12:04:26 +0300

    New teststst fnkwje

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ import './styles.scss';
 const test = [1, 2, 3];
 
 const Page = (props) => (
-    <div styleName="test">Welcome bbb tongggggggggg gggggggggggggggggggg ggggggggggggggggggggggggggggggext.js!!!!</div>
+    <div styleName="test">
+        Welcome bbb tongggggggggg gggggggggggggggggggg ggggggggggggggggggggggggggggggext.js!!!!
+    </div>
 );
 
 export default Page;
