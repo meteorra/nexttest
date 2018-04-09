7c8e29cf36699aea348417d93a46942ec84acd38
Diana Baranova <Diana_Baranova@epam.com>
2018-04-09 11:35:29 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles.scss';
+
+const test = [
+    1,
+    2,
+    3]
+
+const Page = (props) => (
+    <div styleName="test">
+        Welcome bbb tongggggggggg gggggggggggggggggggg
+        ggggggggggggggggggggggggggggggext.js!!!!
+    </div>
+);
+
+export default Page;
